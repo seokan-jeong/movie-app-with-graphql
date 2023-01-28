95a40006504ecb7d12389c920babee0ba195bf55
seokan-jeong <kd04199@gmail.com>
2023-01-29 03:55:30 +0900

    1.3 useQuery

--- movieql-client/src/routes/Movie.js
+++ movieql-client/src/routes/Movie.js
@@ -1,3 +1,24 @@
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      id: id,
+    },
+  });
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,27 +1,32 @@
-import { useApolloClient, gql } from "@apollo/client";
+import { useApolloClient, gql, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  {
+    movies {
+      title
+      id
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          query allMovies {
-            movies {
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.movies));
-  }, []);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
-    <div>
-      {movies.map((movie) => (
-        <li key={movie.title}>{movie.title}</li>
+    <ul>
+      <h1>Movies</h1>
+      {data.movies.map((movie) => (
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
